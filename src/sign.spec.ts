d549a230a0486f8cb99efa8542ae9e2b365d57e6
Vizi Csaba <vizi.csaba89@gmail.com>
2020-04-20 08:22:26 +0200

    :white_check_mark: test and asset init

--- src/sign.spec.ts
+++ src/sign.spec.ts
@@ -64,6 +64,39 @@ describe('some tests', () => {
     fs.writeFileSync('./assets/results/signed-with-image.pdf', signedPdf)
   })
 
+  it('one sign with picture multi page pdf', async () => {
+      const p12Buffer = fs.readFileSync(`./assets/pdf-signer.p12`)
+      const pdfBuffer = fs.readFileSync(`./assets/example2.pdf`)
+
+      const signedPdf = await sign(pdfBuffer, p12Buffer, 'pdfsigner', {
+        reason: '2',
+        email: 'test@email.com',
+        location: 'Location, LO',
+        signerName: 'Test User',
+        annotationAppearanceOptions: {
+          signatureCoordinates: { left: 0, bottom: 700, right: 190, top: 860 },
+          signatureDetails: [
+            {
+              value: 'Signed by: Kiss Béla',
+              fontSize: 7,
+              transformOptions: { rotate: 0, space: 1, tilt: 0, xPos: 20, yPos: 20 },
+            },
+            {
+              value: 'Date: 2019-10-11',
+              fontSize: 7,
+              transformOptions: { rotate: 0, space: 1, tilt: 0, xPos: 20, yPos: 30 },
+            },
+          ],
+          imageDetails: {
+            imagePath: './assets/certification.jpg',
+            transformOptions: { rotate: 0, space: 200, stretch: 50, tilt: 0, xPos: 0, yPos: 10 },
+          },
+        },
+      })
+
+      fs.writeFileSync('./assets/results/signed-with-image-multi-page.pdf', signedPdf)
+    })
+
   it('sign once -> save -> sign again', async () => {
     const p12Buffer = fs.readFileSync(`./assets/pdf-signer.p12`)
     let pdfBuffer = fs.readFileSync(`./assets/example.pdf`)

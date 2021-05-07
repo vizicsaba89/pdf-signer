84c2109aaccc39b3f15fd9381d51d8aa1f43f75d
Cristhian Pinto <crispinalejo16@gmail.com>
2021-05-07 11:51:54 -0500

    Solution to PDF with Links

--- src/sign-original.spec.ts
+++ src/sign-original.spec.ts
@@ -344,7 +344,7 @@ describe('some tests', () => {
     const signedPdf = await sign(pdfBuffer, p12Buffer, 'pdfsigner', {
       reason: '2',
       email: 'test@email.com',
-      location: 'Location, LO',
+      location: 'Location, LO', 
       signerName: 'Test User',
       annotationAppearanceOptions: {
         signatureCoordinates: { left: 0, bottom: 700, right: 190, top: 860 },
@@ -369,4 +369,30 @@ describe('some tests', () => {
 
     fs.writeFileSync('./assets/results/signed-with-interlaced-image.pdf', signedPdf)
   })
+
+  it('signs a ready pdf containing a link', async () => {
+    const p12Buffer = fs.readFileSync(`./assets/pdf-signer.p12`)
+    const pdfBuffer = fs.readFileSync(`./assets/including-a-link.pdf`)
+    
+    const signedPdf = await sign(pdfBuffer, p12Buffer, 'pdfsigner', {
+      reason: 'PDF Link intern',
+      email: 'test@email.com',
+      location: 'Location, LO', 
+      signerName: 'Test User',
+      annotationAppearanceOptions: {
+        signatureCoordinates: { left: 0, bottom: 500, right: 190, top: 660 },
+        signatureDetails: [
+          {
+            value: 'Signed by: Kiss Béla',
+            fontSize: 7,
+            transformOptions: { rotate: 0, space: 1, tilt: 0, xPos: 20, yPos: 20 },
+          },
+          {
+            value: 'Date: 2019-10-11',
+            fontSize: 7,
+            transformOptions: { rotate: 0, space: 1, tilt: 0, xPos: 20, yPos: 30 },
+          },
+        ],
+      },
+    })
 })
